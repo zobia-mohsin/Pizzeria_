215859649873ae8002a0ddd8144acaa9f6624fb8
Zobia Mohsin <zobia_mohsin1@baylor.edu>
2021-12-09 14:46:45 -0600

    Added class Toppings.

--- pizzas/admin.py
+++ pizzas/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Pizza,
+from .models import Pizza
 
 admin.site.register(Pizza)
--- pizzas/models.py
+++ pizzas/models.py
@@ -9,3 +9,12 @@ class Pizza(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Toppings(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
