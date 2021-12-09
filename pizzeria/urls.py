cd559a137c8389c10928012aefa8dbdbf29b10a1
Zobia Mohsin <zobia_mohsin1@baylor.edu>
2021-12-09 15:43:05 -0600

    Welome to Zobia's Pizza base.html

--- pizzeria/urls.py
+++ pizzeria/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pizzas.urls')),
-    path('users/', include('users.urls')),
+    #path('users/', include('users.urls')),
 ]
